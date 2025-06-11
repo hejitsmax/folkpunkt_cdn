83729e9ceed28a2a49358cf902b0b79e16f52ef3
hejitsmax <max.taylor1@ingka.ikea.com>
2025-06-11 14:13:09 +0100

    added waitforelements

--- checka_modifier.js
+++ checka_modifier.js
@@ -2,18 +2,25 @@ console.log("FOLKPUNKT JS Loaded");
 
 (function() {
     console.log("folkpunkt_cdn function started");
-    function runModifier() {
-        console.log("runModifier called");
+
+    function tryModify() {
+        let found = false;
 
         // 1. Remove the "Continue to shop" button
         const continueShopBtn = document.querySelector('button.btn--emphasised[aria-label="Continue to shop"]');
         console.log("continueShopBtn", continueShopBtn);
-        if (continueShopBtn) continueShopBtn.remove();
+        if (continueShopBtn) {
+            continueShopBtn.remove();
+            found = true;
+        }
 
         // 2. Change button text to 'Start order on FOLKPUNKT'
         const orderButton = document.querySelector('button.btn--secondary[aria-label="Order on behalf of co-worker"] .btn__label');
         console.log("orderButton", orderButton);
-        if (orderButton) orderButton.textContent = 'Start order on FOLKPUNKT';
+        if (orderButton) {
+            orderButton.textContent = 'Start order on FOLKPUNKT';
+            found = true;
+        }
 
         // 3. Change welcome message
         const welcomeHeading = document.querySelector('.checka-order_welcome_text_wrapper h1');
@@ -22,6 +29,7 @@ console.log("FOLKPUNKT JS Loaded");
             welcomeHeading.textContent = 'Welcome to Checka on FOLKPUNKT';
             welcomeHeading.setAttribute('aria-label', 'Welcome to Checka on FOLKPUNKT');
             welcomeHeading.setAttribute('title', 'Welcome to Checka on FOLKPUNKT');
+            found = true;
         }
 
         // 4. Change start instructions
@@ -31,12 +39,16 @@ console.log("FOLKPUNKT JS Loaded");
             startInstructions.textContent = 'Get started by clicking start order.';
             startInstructions.setAttribute('aria-label', 'Get started by clicking start order.');
             startInstructions.setAttribute('title', 'Get started by clicking start order.');
+            found = true;
         }
 
         // 5. Remove tabs container
         const tabsContainer = document.querySelector('.cwds-tabs__container.cwds-header__tabs');
         console.log("tabsContainer", tabsContainer);
-        if (tabsContainer) tabsContainer.remove();
+        if (tabsContainer) {
+            tabsContainer.remove();
+            found = true;
+        }
 
         // 6. Remove user avatar button
         const userAvatarBtn = document.querySelector('button.btn--small.btn--plain .avatar__sr-text');
@@ -45,11 +57,20 @@ console.log("FOLKPUNKT JS Loaded");
             const parentBtn = userAvatarBtn.closest('button');
             console.log("parentBtn", parentBtn);
             if (parentBtn) parentBtn.remove();
+            found = true;
         }
+
+        return found;
     }
-    if (document.readyState === "loading") {
-        document.addEventListener("DOMContentLoaded", runModifier);
-    } else {
-        runModifier();
+
+    function waitForElements() {
+        if (tryModify()) {
+            console.log("FOLKPUNKT: Modifications applied.");
+            return;
+        }
+        // Keep trying every 500ms until at least one element is found and modified
+        setTimeout(waitForElements, 500);
     }
+
+    waitForElements();
 })();
