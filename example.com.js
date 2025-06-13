f6cc849ac7d29fe34793105b85aaf8a746e7e0de
hejitsmax <max.taylor1@ingka.ikea.com>
2025-06-13 18:15:36 +0100

    -

--- example.com.js
+++ example.com.js
@@ -1,21 +1,30 @@
-console.log("FOLKPUNKT H1 JS Loaded");
+console.log("FOLKPUNKT H1 override loaded");
 
 (function () {
-    console.log("folkpunkt_h1 function started");
-
-    const observer = new MutationObserver(() => {
+    function updateH1() {
         const h1 = document.querySelector('h1');
-
         if (h1 && h1.textContent !== 'FOLKPUNKT Test') {
             h1.textContent = 'FOLKPUNKT Test';
             h1.setAttribute('aria-label', 'FOLKPUNKT Test');
             h1.setAttribute('title', 'FOLKPUNKT Test');
             console.log("FOLKPUNKT: H1 text modified.");
+            return true;
         }
-    });
+        return false;
+    }
 
-    observer.observe(document.body, {
-        childList: true,
-        subtree: true
-    });
+    // Run once in case the element is already loaded
+    if (!updateH1()) {
+        // Use MutationObserver in case it loads in after
+        const observer = new MutationObserver(() => {
+            if (updateH1()) {
+                observer.disconnect();
+            }
+        });
+
+        observer.observe(document.body, {
+            childList: true,
+            subtree: true
+        });
+    }
 })();
